fe689ad455dd5e48e778a51da91ec1545a165c45
kogamitsuhiro <utirn.ame@gmail.com>
2018-09-24 12:26:55 +0900

    MemoDetailScreenのStyle上書きを実装

--- src/elements/CircleButton.js
+++ src/elements/CircleButton.js
@@ -3,9 +3,19 @@ import { StyleSheet, View, Text } from 'react-native';
 
 export default class CircleButton extends React.Component {
     render() {
+        const { style, color } = this.props;
+
+        let bgColor = '#E31676';
+        let textColor = '#fff';
+
+        if (color === 'white') {
+            bgColor = 'white';
+            textColor = '#E31676';
+        }
+
         return (
-            <View style={styles.circleButton}>
-                <Text style={styles.circleButtonTitle}>
+            <View style={[styles.circleButton, style,  { backgroundColor: bgColor }]}>
+                <Text style={[styles.circleButtonTitle, { color: textColor }]}>
                     {this.props.children}
                 </Text>
             </View>
