90c1ff304bb8f2e323df2f3507492d43e9be9e98
kogamitsuhiro <utirn.ame@gmail.com>
2018-09-24 10:49:54 +0900

    memolistを作成

---
+++ src/elements/BodyText.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { StyleSheet, View, Text } from 'react-native'; 
+
+export default class BodyText extends React.Component {
+    render() {
+        return (
+            <View>
+                <Text style={styles.text}>
+                    {this.props.children}
+                </Text>
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    text: {
+      color: 'red',
+      backgroundColor: '#eee',
+    },
+});
