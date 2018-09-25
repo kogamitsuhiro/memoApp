29054180d2db9adb104bcec08527b1f3681030ba
kogamitsuhiro <utirn.ame@gmail.com>
2018-09-25 20:22:13 +0900

    SignupScreenを作成した

---
+++ src/screens/SignupScreen.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import { StyleSheet, View, Text, TextInput, Button, TouchableHighlight } from 'react-native'; 
+
+import CircleButton from '../elements/CircleButton';
+
+export default class SignupScreen extends React.Component {
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text style={styles.title}>メンバー登録</Text>
+                <TextInput style={styles.input} value="Email address"/>
+                <TextInput style={styles.input} value="Password"/>
+                <TouchableHighlight style={styles.button} title="ログイン" onPress={ () => {}} underlayColor="#c70f66">
+                    <Text style={styles.buttonTitle}>
+                        送信する
+                    </Text>
+                </TouchableHighlight>
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      width: '100%',
+      padding: 24,
+      backgroundColor: '#fff',
+    },
+    input: {
+        backgroundColor: '#eee',
+        height: 48,
+        marginBottom: 16,
+        borderWidth: 1,
+        borderColor: '#ddd',
+        padding: 8,
+    },
+    title: {
+        fontSize: 24,
+        alignSelf: 'center',
+        marginBottom: 24,
+    },
+    button: {
+        backgroundColor: '#e31676',
+        height: 48,
+        borderRadius: 4,
+        justifyContent: 'center',
+        alignItems: 'center',
+        alignSelf: 'center',
+        width: '70%',
+    },
+    buttonTitle: {
+        color: '#fff',
+        fontSize: 18,
+    }
+}); 
