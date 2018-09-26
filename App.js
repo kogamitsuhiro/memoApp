d3e8e087d69004289d2748494753c401337f37c2
kogamitsuhiro <utirn.ame@gmail.com>
2018-09-26 20:41:45 +0900

    各画面をnavigationScreenに登録した

--- App.js
+++ App.js
@@ -3,11 +3,17 @@ import { StyleSheet, View } from 'react-native';
 import { StackNavigator } from 'react-navigation';
 
 import MemoListScreen from './src/screens/MemoListScreen';
+import MemoDetailScreen from './src/screens/MemoDetailScreen';
+import MemoEditScreen from './src/screens/MemoEditScreen';
+import LoginScreen from './src/screens/LoginScreen';
+import SignupScreen from './src/screens/SignupScreen';
 
 const App = StackNavigator({
-  Home: {
-    screen: MemoListScreen,
-  },
+  Home: { screen: MemoListScreen },
+  MemoDetail: { screen: MemoDetailScreen},
+  MemoEdit: { screen: MemoEditScreen},
+  Login: { screen: LoginScreen},
+  Signup: { screen: SignupScreen},
 }, {
   navigationOptions: {
     headerTitle: 'Memot',
