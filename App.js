cb00c5a858d6b2089849ddb899b0f1dcdebb95e8
kogamitsuhiro <utirn.ame@gmail.com>
2018-10-06 10:07:51 +0900

    firebaseの初期化

--- App.js
+++ App.js
@@ -1,6 +1,5 @@
-import React from 'react';
-import { StyleSheet, View } from 'react-native';
 import { createStackNavigator } from 'react-navigation';
+import firebase from 'firebase';
 
 import MemoListScreen from './src/screens/MemoListScreen';
 import MemoDetailScreen from './src/screens/MemoDetailScreen';
@@ -8,9 +7,21 @@ import MemoEditScreen from './src/screens/MemoEditScreen';
 import LoginScreen from './src/screens/LoginScreen';
 import SignupScreen from './src/screens/SignupScreen';
 
+import ENV from './env.json';
+
 // ↓これを書かないとエラーが出る
 // https://github.com/apollographql/apollo-link/issues/75#issuecomment-333328069
 GLOBAL.self = GLOBAL;
+
+const config = {
+  apiKey: ENV.FIREBASE_API_KEY,
+  authDomain: ENV.FIREBASE_AUTH_DOMAIN,
+  databaseURL: ENV.FIREBASE_DB_URL,
+  projectId: ENV.FIREBASE_PROJECT_ID,
+  storageBucket: ENV.FIREBASE_STORAGE,
+  messagingSenderId: ENV.FIREBASE_SENDER_ID,
+};
+firebase.initializeApp(config);
 
 const App = createStackNavigator({
   Login: LoginScreen,
